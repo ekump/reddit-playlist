ca335f8004512e96211ff2221e3c700aefea669c
Edmund Kump <edmund@shakelaw.com>
2017-07-17 00:00:35 -0400

    wip

---
+++ src/services/reddit.service.spec.ts
@@ -0,0 +1,36 @@
+import { Http, RequestOptions, Headers, ResponseContentType } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { Observable } from 'rxjs/Rx';
+import { RedditService } from '../services';
+
+let redditService: RedditService;
+let backend = new MockBackend();
+let requestOptions = new RequestOptions({});
+let http: Http;
+
+describe('RedditService', () => {
+  describe('#getSubReddits', () => {
+    beforeEach(() => {
+      http = new Http(backend, requestOptions);
+    });
+
+    it('returns an observable with an array of subreddits ', done => {
+      let resp  = {
+        json() {
+          let data = {};
+          data['data'] = {};
+          data['data']['content_md'] = "##Rock/Metal\n* /r/blackMetal";
+          return data;
+        }
+      };
+      let observable = Observable.from([resp]);
+      spyOn(http, 'get').and.returnValue(observable);
+      redditService = new RedditService(http);
+      let getSubRedditsObservable = redditService.getSubReddits();
+      getSubRedditsObservable.subscribe((response) => {
+        expect(response).toEqual(redditService.subReddits);
+        done();
+      });
+    });
+  });
+});
