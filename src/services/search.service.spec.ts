8ad458716bf1deedb725bdecfd86ad73eb562ec0
Edmund Kump <edmund@shakelaw.com>
2017-08-09 21:29:33 -0400

    adding more tests

--- src/services/search.service.spec.ts
+++ src/services/search.service.spec.ts
@@ -19,11 +19,42 @@ describe('SearchService', () => {
     spotifyService = new SpotifyService(http);
     searchService = new SearchService(spotifyService);
   });
-  describe('searchForSongs', () => {
+  describe('#searchForSongs', () => {
     it('calls the spotify service for each post', done => {
-      let spotifySearchSpy = spyOn(spotifyService, 'search').and.returnValue(Observable.from([[new SpotifyTrack(SpotifyTrackFactory.build())]]));
+      let spotifySearchSpy = spyOn(spotifyService, 'search').and.returnValue(Observable.of([new SpotifyTrack(SpotifyTrackFactory.build())]));
       searchService.searchForSongs(posts);
       expect(spotifySearchSpy.calls.count()).toBe(2);
+      done();
+    });
+  });
+
+  describe('#matchSearchResults', () => {
+    it('matches a post to a spotify track that has been returned as part of the search', done => {
+      let spotifyTracks = [new SpotifyTrack(SpotifyTrackFactory.build())];
+      let post = 'Test Artist - Test Track'
+      let actualSpotifyTracks = searchService.matchSearchResults(post, spotifyTracks);
+
+      expect(actualSpotifyTracks.length).toBe(1);
+      expect(actualSpotifyTracks[0].name).toBe(spotifyTracks[0].name);
+      done();
+    });
+
+    it('matches when post has spaces or different capitalization', done => {
+      let spotifyTracks = [new SpotifyTrack(SpotifyTrackFactory.build())];
+      let post = 'Test arTist - Test TRAck   '
+      let actualSpotifyTracks = searchService.matchSearchResults(post, spotifyTracks);
+
+      expect(actualSpotifyTracks.length).toBe(1);
+      expect(actualSpotifyTracks[0].name).toBe(spotifyTracks[0].name);
+      done();
+    });
+
+    it('rejects tracks where song title matches but artist does not', done => {
+      let spotifyTracks = [new SpotifyTrack(SpotifyTrackFactory.build())];
+      let post = 'Galaxie 500 - Test Track'
+      let actualSpotifyTracks = searchService.matchSearchResults(post, spotifyTracks);
+
+      expect(actualSpotifyTracks.length).toBe(0);
       done();
     });
   });
