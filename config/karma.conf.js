a91129580a4a3b76454f8f604bd4196909610a3a
Edmund Kump <edmund@shakelaw.com>
2017-08-12 17:27:45 -0400

    adding code coverage tool, setting threshold to current values so error isn't thrown. Will increase where possible.
    LGTM EK
    
    closes #7

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -5,24 +5,46 @@ const TEST_WEBPACK_CONFIG = require('./webpack.test');
 module.exports = function(config) {
   let configuration = {
     basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'source-map-support'],
     exclude: [ ],
     files: [
       { pattern: './src/test/test.component.html', nocache: true },
       { pattern: './config/spec-bundle.js', watched: false }
     ],
-    preprocessors: { './config/spec-bundle.js': ['webpack'] },
+    preprocessors: {
+      './config/spec-bundle.js': ['webpack'],
+      './src/**/*.spec.ts': ['webpack']
+    },
+    reporters: [ 'mocha', 'coverage', 'coverage-istanbul' ],
     webpack: TEST_WEBPACK_CONFIG,
     coverageReporter: {
-      type: 'in-memory'
+      reporters: [
+        {
+          type: 'json',
+          dir: 'coverage/json',
+          subdir: '.'
+        }
+      ]
     },
-    remapCoverageReporter: {
-      'text-summary': null,
-      json: './coverage/coverage.json',
-      html: './coverage/html'
+    coverageIstanbulReporter: {
+      reports: ['text', 'html' ],
+      thresholds: {
+        emitWarning: false,
+        global: {
+          statements: 94,
+          lines: 93,
+          branches: 77,
+          functions: 90
+        },
+        each: {
+          statements: 88,
+          lines: 88,
+          branches: 66,
+          functions: 80
+        }
+      }
     },
     webpackMiddleware: { stats: 'errors-only'},
-    reporters: [ 'mocha', 'coverage' ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
